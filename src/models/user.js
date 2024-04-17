61460d202326f267aa4c8febce22d36b8dff60bc
Natalia Córdoba <nataliacorrdoba@gmail.com>
2024-04-17 20:57:46 -0300

    segunda practica integradora

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import cartModel from './cart.js';
 
 const userSchema = new Schema({
     first_name: {
@@ -26,6 +27,30 @@ const userSchema = new Schema({
         type: String,
         default: "User"
     },
+    cart_id: {
+        type: Schema.Types.ObjectId,
+        ref:'carts'
+    }
+})
+
+userSchema.pre('save', async function (next) {
+    try {
+        const newCart = await cartModel.create({ products: [] })
+        console.log(newCart)
+        this.cart_id = newCart._id
+    } catch (error) {
+        next(error)        
+    }
+})
+
+userSchema.pre('find', async function (next) {
+    try {
+        const prods = await cartModel.findOne({ _id: '661ffdb871b1d8213394c0e6' })
+        console.log(prods)
+        this.populate('cart_id')
+    } catch (error) {
+        next(error)
+    }
 })
 
 const userModel = model("users", userSchema)
